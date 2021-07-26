7397386ee51e0115b11465baaecbd60c44f14741
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-26 07:35:24 -0400

    began working on CRUD functionality. Update user still needs work

--- routes/auth.js
+++ routes/auth.js
@@ -24,4 +24,24 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User Not Found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    //validate Password
+    !validPassword && res.status(400).json("Password is incorrect");
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
